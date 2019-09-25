215159682a232a3a2a69da51f77b01fcf37ebf66
Yerlin Matu <maicolmatu27@gmail.com>
2019-09-24 19:24:16 -0500

    Learned about loops

---
+++ loops.py
@@ -0,0 +1,15 @@
+for item in range(5):
+    print(item)
+
+
+print('pairs number from 2 to 50')
+
+for item in range(50):
+    if (item % 2 == 0):
+        print(item)
+
+
+print('Show each letter of string')
+
+for letter in 'Think':
+    print(letter)
