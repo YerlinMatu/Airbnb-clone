2d636d96805663802ff79f09bdab337ee861d9e0
Yerlin Matu <maicolmatu27@gmail.com>
2019-11-09 23:23:49 -0500

    Added kwargs to base model

--- models/base_model.py
+++ models/base_model.py
@@ -2,26 +2,33 @@
 from uuid import uuid4
 from datetime import datetime
 
-
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
-    def __str__(self):
-      return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                setattr(self, key, value)
+                if key == 'created_at':
+                    self.created_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == 'update_at':
+                    self.updated_at = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def _str_(self):
+        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
+
         new_dict = self.__dict__.copy()
-        new_dict.update(
-            {
-              'created_at': datetime.isoformat(self.created_at),
-              'updated_at': datetime.isoformat(self.created_at),
-              '__class__': self.__class__.__name__
-            }
-        )
+        new_dict.update({'created_at': datetime.isoformat(self.created_at)})
+        new_dict.update({'update_at': datetime.isoformat(self.updated_at)})
+        new_dict.update({'__class__': self.__class__.__name__})
         return new_dict
