e6a99f3bd7fb2aa1d0e6215c5987a9488cc44300
Yerlin Matu <maicolmatu27@gmail.com>
2019-11-10 22:15:57 -0500

    Fix: documentation quit command

--- console.py
+++ console.py
@@ -2,10 +2,11 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    prompt = ' (hbnb) '
+    prompt = '(hbnb) '
 
     def do_quit(self, args):
-        'Quit command to exit the program'
+        """Quit command to exit the program
+        """
         return True
 
     def do_EOF(self, args):
