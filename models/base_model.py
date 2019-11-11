40248bc1d32897cb3867fb9d550d39d98a649717
Yerlin Matu <maicolmatu27@gmail.com>
2019-11-10 19:21:58 -0500

    Added engine and class file storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+import models
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
@@ -19,18 +19,23 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
+            models.storage.save()
 
-    def _str_(self):
+    def __str__(self):
         return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         new_dict = self.__dict__.copy()
-        new_dict.update({
-            'created_at': datetime.isoformat(self.created_at),
-            'update_at': datetime.isoformat(self.updated_at),
-            '__class__': self.__class__.__name__
-        })
+        if 'created_at' in new_dict:
+            new_dict.update({'created_at': 
+                new_dict['created_at'].strftime('%Y-%m-%dT%H:%M:%S.%f')})
+        if 'updated_at' in new_dict:
+            new_dict.update({'updated_at':
+                new_dict['updated_at'].strftime('%Y-%m-%dT%H:%M:%S.%f')})
+        new_dict.update({'__class__': self.__class__.__name__})
         return new_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import json
+import models
+from models.base_model import BaseModel
+
+classes = {'BaseModel': BaseModel}
+
+class FileStorage:
+    def __init__(self):
+        self.__file_path = 'file.json'
+        self.__objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        main_key = obj.__class__.__name__ + '.' + obj.id
+        self.__objects.update({
+            main_key : obj
+        })
+
+    def save(self):
+        new_dict = {}
+
+        for key in self.__objects:
+            new_dict[key] = self.__objects[key].to_dict()
+
+        with open(self.__file_path, 'w') as f:
+            json.dump(new_dict, f, indent=4)
+
+    def reload(self):
+        try:
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+            for key in data:
+                self.__objects[key] = classes[data[key]['__class__']](**data[key])
+        except:
+            pass
