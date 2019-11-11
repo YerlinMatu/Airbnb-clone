b294f46d4bdcb9203a692046de78b6860e7c4a8e
Yerlin Matu <maicolmatu27@gmail.com>
2019-11-10 21:00:45 -0500

    Feat: intepreter CMD

---
+++ console.py
@@ -0,0 +1,21 @@
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = ' (hbnb) '
+
+    def do_quit(self, args):
+        'Quit command to exit the program'
+        return True
+
+    def do_EOF(self, args):
+        'Exits command console'
+        return True
+
+    def emptyline(self):
+        'An empty line + ENTER should not execute anything'
+        return False
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
